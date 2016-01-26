beddce3e14054d5425fe42206b3ad82affc004ed
amitthakur2014 <amit-thakur@hotmail.com>
2016-01-26 11:17:24 +0530

    Added Solution to GitHub

---
+++ MvcApplication.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvcApplication.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual= string.Empty;
+            //actual = target.combineArrayStringWithSpace(stringArray);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
